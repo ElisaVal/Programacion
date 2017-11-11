ed065a65fa662583f8866d7b906ffeccdb25f9e4
ElisaVal <33353269+ElisaVal@users.noreply.github.com>
2017-11-10 22:10:23 -0600

    WSQ 01 – Fun with Numbers
    WSQ 01 – Fun with Numbers

---
+++ num.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num1, num2;
+	cout<<"inserta el primer numero";
+	cin>>num1;
+	cout<<"inserta el segundo numero";
+	cin>>num2;
+	cout<<"la suma es ";
+	cout<<num1+num2 << endl ;
+	cout<<"la resta es ";
+	cout<<num1-num2<< endl;
+	cout<<"la multiplicacion es ";
+	cout<<num1*num2<< endl;
+	cout<<"la divison es ";
+	cout<<num1/num2<< endl;
+	cout<<"el residuo es ";
+	cout<<num1%num2<< endl;
+	
+	// your code goes here
+	return 0;
+}
